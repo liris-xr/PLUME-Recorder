bbe1c967bf23775beb9e81b1f4c226caaf9041c6
Charles JAVERLIAT <charles.javerliat@gmail.com>
2024-02-09 22:36:10 +0100

    Removed BurstCompile attributes on SampleTypeUrlRegistry methods

---
+++ Runtime/Core/Recorder/Module/RecorderModuleRegistry.cs
@@ -0,0 +1,7 @@
+namespace PLUME.Core.Recorder.Module
+{
+    public class RecorderModuleRegistry
+    {
+        
+    }
+}
--- Runtime/Core/Recorder/SampleTypeUrlRegistry.cs
+++ Runtime/Core/Recorder/SampleTypeUrlRegistry.cs
@@ -1,6 +1,5 @@
 using System;
 using System.Collections.Generic;
-using Unity.Burst;
 using Unity.Collections;
 
 namespace PLUME.Core.Recorder
@@ -8,11 +7,11 @@ namespace PLUME.Core.Recorder
     public static class SampleTypeUrlRegistry
     {
         private static int _nextId;
+
         // TODO: add multiple bin depending on the size of the string. This would enable burst compiled sample packing for most of the samples
         private static readonly Dictionary<FixedString128Bytes, SampleTypeUrlIndex> TypeUrlToIndex = new();
         private static readonly Dictionary<SampleTypeUrlIndex, FixedString128Bytes> IndexToTypeUrl = new();
 
-        [BurstCompile]
         internal static void RegisterTypeUrl(FixedString128Bytes typeUrl)
         {
             if (TypeUrlToIndex.TryGetValue(typeUrl, out var index))
@@ -25,7 +24,6 @@ namespace PLUME.Core.Recorder
             IndexToTypeUrl.Add(index, typeUrl);
         }
 
-        [BurstCompile]
         public static SampleTypeUrlIndex GetTypeUrlIndex(FixedString128Bytes typeUrl)
         {
             if (!TypeUrlToIndex.TryGetValue(typeUrl, out var index))
@@ -36,7 +34,6 @@ namespace PLUME.Core.Recorder
             return index;
         }
 
-        [BurstCompile]
         public static FixedString128Bytes GetTypeUrlFromIndex(SampleTypeUrlIndex index)
         {
             if (!IndexToTypeUrl.TryGetValue(index, out var typeUrl))

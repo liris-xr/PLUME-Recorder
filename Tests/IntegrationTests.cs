d4d36f48b37dbc0b8ede7a4618f229e258249635
Javerliat Charles <charles.javerliat@gmail.com>
2024-02-12 11:22:55 +0100

    Removed unused commented code from test

--- Tests/IntegrationTests.cs
+++ Tests/IntegrationTests.cs
@@ -27,22 +27,5 @@ namespace PLUME.Tests
 
             yield return null;
         }
-
-        // [UnityTest]
-        // public IEnumerator IntegrationTest() => UniTask.ToCoroutine(async () =>
-        // {
-        //     var objectSafeRefProvider = new ObjectSafeRefProvider();
-        //
-        //     var go1 = new GameObject("go1");
-        //     var go2 = new GameObject("go2");
-        //     var tRef1 = objectSafeRefProvider.GetOrCreateTypedObjectSafeRef(go1.transform);
-        //     var tRef2 = objectSafeRefProvider.GetOrCreateTypedObjectSafeRef(go2.transform);
-        //     
-        //     PlumeRecorder.Instance.Start();
-        //     PlumeRecorder.Instance.StartRecordingObject(tRef1, true);
-        //     PlumeRecorder.Instance.StartRecordingObject(tRef2, true);
-        //     PlumeRecorder.Instance.RecordMarker("Start");
-        //     PlumeRecorder.Instance.Stop();
-        // });
     }
 }

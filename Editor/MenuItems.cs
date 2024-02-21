74e275ad4532338b3fb446e6f75baa6e1b9bef76
Charles JAVERLIAT <charles.javerliat@gmail.com>
2024-02-21 22:36:18 +0100

    Added menu items

--- Editor/MenuItems.cs
+++ Editor/MenuItems.cs
@@ -1,13 +1,26 @@
 using UnityEditor;
+using UnityEngine;
 
 namespace PLUME.Editor
 {
     public static class MenuItems
     {
-        [MenuItem("PLUME/Build Asset Bundle")]
+        [MenuItem("PLUME/Build Asset Bundle", priority = 0)]
         private static void BuildAssetBundle()
         {
             AssetBundleBuilder.BuildAssetBundle();
+        }
+        
+        [MenuItem("PLUME/GitHub Repository", priority = 20)]
+        private static void OpenGitHubRepository()
+        {
+            Application.OpenURL("https://www.github.com/liris-xr/PLUME");
+        }
+        
+        [MenuItem("PLUME/About...", priority = 21)]
+        private static void OpenAbout()
+        {
+            Application.OpenURL("https://www.github.com/liris-xr/PLUME");
         }
     }
 }

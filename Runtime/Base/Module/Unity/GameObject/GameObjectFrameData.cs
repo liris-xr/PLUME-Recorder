e1241de612e76300676de88fc11807b045f0904f
Javerliat Charles <charles.javerliat@gmail.com>
2024-02-22 16:53:36 +0100

    Added GameObject recorder module

---
+++ Runtime/Base/Module/Unity/GameObject/GameObjectFrameData.cs
@@ -0,0 +1,12 @@
+using PLUME.Core.Recorder.Module.Frame;
+
+namespace PLUME.Base.Module.Unity.GameObject
+{
+    public struct GameObjectFrameData : IFrameData
+    {
+        public void Serialize(FrameDataWriter frameDataWriter)
+        {
+            
+        }
+    }
+}
---
+++ Runtime/Base/Module/Unity/GameObject/GameObjectRecorderModule.cs
@@ -0,0 +1,12 @@
+using PLUME.Core.Recorder.Module.Frame;
+
+namespace PLUME.Base.Module.Unity.GameObject
+{
+    public class GameObjectRecorderModule : ObjectFrameDataRecorderModuleBase<UnityEngine.GameObject, GameObjectFrameData>
+    {
+        protected override GameObjectFrameData CollectFrameData(FrameInfo frameInfo)
+        {
+            return new GameObjectFrameData();
+        }
+    }
+}

ebd9b7d9c71c5e1d40ef9e83186a5a9eff73e8a8
Javerliat Charles <charles.javerliat@gmail.com>
2024-02-22 09:12:22 +0100

    Added GetBatchingForPlatform in AssetBundleBuilder

--- Editor/AssetBundleBuilder.cs
+++ Editor/AssetBundleBuilder.cs
@@ -12,6 +12,7 @@ using UnityEngine.Rendering;
 using UnityEngine.SceneManagement;
 using UnityRuntimeGuid.Editor;
 using CompressionLevel = System.IO.Compression.CompressionLevel;
+using Logger = PLUME.Core.Logger;
 
 namespace PLUME.Editor
 {
@@ -23,9 +24,13 @@ namespace PLUME.Editor
             EditorApplication.playModeStateChanged += state =>
             {
                 if (state != PlayModeStateChange.ExitingEditMode) return;
-                
-                SetBatchingForPlatform(EditorUserBuildSettings.activeBuildTarget, 0, 0);
-                
+
+                GetBatchingForPlatform(EditorUserBuildSettings.activeBuildTarget, out var staticBatching,
+                    out var dynamicBatching);
+
+                if (staticBatching != 0 || dynamicBatching != 0)
+                    SetBatchingForPlatform(EditorUserBuildSettings.activeBuildTarget, 0, 0);
+
                 var activeScene = SceneManager.GetActiveScene();
                 EditorSceneManager.MarkSceneDirty(activeScene);
                 EditorSceneManager.SaveScene(activeScene);
@@ -34,20 +39,25 @@ namespace PLUME.Editor
                 GuidRegistryUpdater.UpdateAssetsGuidRegistry(GuidRegistryUpdater.GetAllScenePaths(true));
                 GuidRegistryUpdater.UpdateScenesGuidRegistry(GuidRegistryUpdater.GetAllScenePaths(true));
             };
-            
+
             BuildPlayerWindow.RegisterBuildPlayerHandler(buildPlayerOptions =>
             {
                 // Update the registries when building the application
                 GuidRegistryUpdater.UpdateAssetsGuidRegistry(GuidRegistryUpdater.GetAllScenePaths(false));
                 GuidRegistryUpdater.UpdateScenesGuidRegistry(GuidRegistryUpdater.GetAllScenePaths(false));
+
+                GetBatchingForPlatform(buildPlayerOptions.target, out var staticBatching,
+                    out var dynamicBatching);
+
+                if (staticBatching != 0 || dynamicBatching != 0)
+                    SetBatchingForPlatform(buildPlayerOptions.target, 0, 0);
                 
-                SetBatchingForPlatform(buildPlayerOptions.target, 0, 0);
                 BuildAssetBundle();
                 BuildPlayerWindow.DefaultBuildMethods.BuildPlayer(buildPlayerOptions);
             });
         }
-        
-        
+
+
         public static void BuildAssetBundle()
         {
             var scenePaths = EditorBuildSettings.scenes
@@ -61,7 +71,7 @@ namespace PLUME.Editor
             {
                 AddAssetWithDependencies(assetsPaths, scenePath);
             }
-            
+
             if (GraphicsSettings.defaultRenderPipeline != null)
             {
                 var defaultRenderPipelineAssetPath = AssetDatabase.GetAssetPath(GraphicsSettings.defaultRenderPipeline);
@@ -75,19 +85,19 @@ namespace PLUME.Editor
                 var qualityLevelRenderPipelineAssetPath = AssetDatabase.GetAssetPath(qualityLevelRenderPipeline);
                 AddAssetWithDependencies(assetsPaths, qualityLevelRenderPipelineAssetPath);
             }
-            
+
             var assetsBuild = new AssetBundleBuild
             {
                 assetBundleName = "plume_assets",
                 assetNames = assetsPaths.ToArray()
             };
-            
+
             var scenesBuild = new AssetBundleBuild
             {
                 assetBundleName = "plume_scenes",
                 assetNames = scenePaths.ToArray()
             };
-            
+
             var outputPath = Path.Join(Application.dataPath, "AssetBundles/plume_bundle/");
             var zipOutputPath = Path.Join(Application.streamingAssetsPath, "plume_bundle.zip");
             var builds = new[] { assetsBuild, scenesBuild };
@@ -96,29 +106,55 @@ namespace PLUME.Editor
 
             Directory.CreateDirectory(outputPath);
             CompatibilityBuildPipeline.BuildAssetBundles(outputPath, builds, options, target);
-            
+
             File.Delete(zipOutputPath);
             ZipFile.CreateFromDirectory(outputPath, zipOutputPath, CompressionLevel.Optimal, false);
         }
 
         private static void SetBatchingForPlatform(BuildTarget platform, int staticBatching, int dynamicBatching)
         {
-            Debug.Log("Disabling static and dynamic batching for PLUME.");
-            var method = typeof(PlayerSettings).GetMethod("SetBatchingForPlatform", BindingFlags.Static | BindingFlags.Default | BindingFlags.NonPublic);
-   
-            if (method == null)
+            Logger.Log("Disabling static and dynamic batching for PLUME.");
+
+            var setter = typeof(PlayerSettings).GetMethod("SetBatchingForPlatform",
+                BindingFlags.Static | BindingFlags.Default | BindingFlags.NonPublic);
+
+            if (setter == null)
             {
                 throw new NotSupportedException("Setting batching per platform is not supported");
             }
- 
+
             var args = new object[]
             {
                 platform,
                 staticBatching,
                 dynamicBatching
             };
- 
-            method.Invoke(null, args);
+
+            setter.Invoke(null, args);
+        }
+
+        private static void GetBatchingForPlatform(BuildTarget platform, out int staticBatching,
+            out int dynamicBatching)
+        {
+            var getter = typeof(PlayerSettings).GetMethod("GetBatchingForPlatform",
+                BindingFlags.Static | BindingFlags.Default | BindingFlags.NonPublic);
+
+            if (getter == null)
+            {
+                throw new NotSupportedException("Getting batching per platform is not supported");
+            }
+
+            var args = new object[]
+            {
+                platform,
+                null,
+                null
+            };
+
+            getter.Invoke(null, args);
+
+            staticBatching = (int)args[1];
+            dynamicBatching = (int)args[2];
         }
 
         private static bool CanAddAsset(ICollection<string> assetPaths, string assetPath)
@@ -130,17 +166,17 @@ namespace PLUME.Editor
 
             if (assetType == null)
                 return false;
-            
+
             return assetType.Namespace == null || !assetType.Namespace.StartsWith("UnityEditor");
         }
-        
+
         private static void AddAssetWithDependencies(ICollection<string> assetPaths, string assetPath)
         {
             if (CanAddAsset(assetPaths, assetPath))
             {
                 assetPaths.Add(assetPath);
             }
-            
+
             var dependenciesPaths = AssetDatabase.GetDependencies(assetPath, true);
 
             foreach (var dependencyPath in dependenciesPaths)

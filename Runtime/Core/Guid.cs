4f25e513288c9afc46fae37e23f4d2a1c1ece0ae
Javerliat Charles <charles.javerliat@gmail.com>
2024-04-12 17:25:47 +0200

    Remove FixedString from Guid struct

--- Runtime/Core/Guid.cs
+++ Runtime/Core/Guid.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text;
 using ProtoBurst;
 using ProtoBurst.Packages.ProtoBurst.Runtime;
 using Unity.Burst;
@@ -20,44 +21,74 @@ namespace PLUME.Core
 
         private readonly ulong _a;
         private readonly ulong _b;
-        private FixedString64Bytes _utf8String;
 
         public Guid(ulong a, ulong b) {
             _a = a;
             _b = b;
-            _utf8String = new FixedString64Bytes(_a.ToString("X16") + _b.ToString("X16"));
         }
         
-        public static Guid FromString(string str)
+        public static Guid FromString(string guid)
         {
-            var systemGuid = System.Guid.Parse(str);
-            return FromSystemGuid(systemGuid);
-        }
-        
-        public static Guid FromSystemGuid(System.Guid systemGuid)
-        {
-            var bytes = systemGuid.ToByteArray();
-            var a = BitConverter.ToUInt64(bytes, 0);
-            var b = BitConverter.ToUInt64(bytes, 8);
-            return new Guid(a, b);
-        }
+            var formattedStr = System.Guid.Parse(guid).ToString("N");
+            
+            ulong a = 0;
+            ulong b = 0;
+            
+            for (var i = 0; i < formattedStr.Length; i++)
+            {
+                var c = formattedStr[i];
 
-        [BurstCompile]
-        private static bool IsHexChar(ref Unicode.Rune rune)
-        {
-            return rune.value is >= '0' and <= '9' or >= 'a' and <= 'f' or >= 'A' and <= 'F';
+                if (i < 16)
+                    a |= (ulong)CharToHex(c) << (i * 4);
+                else
+                    b |= (ulong)CharToHex(c) << (i % 16 * 4);
+            }
+            
+            return new Guid(a, b);
         }
 
         [BurstCompile]
         public void WriteTo(ref BufferWriter bufferWriter)
         {
-            bufferWriter.WriteFixedString(ref _utf8String);
+            for(var i = 0; i < 32; i++)
+            {
+                if (i < 16)
+                {
+                    var shift = i * 4;
+                    var val = (byte) ((_a >> shift) & 0xF);
+                    bufferWriter.WriteByte((byte) HexToChar(val));
+                }
+                else
+                {
+                    var shift = (i - 16) * 4;
+                    var val = (byte) ((_b >> shift) & 0xF);
+                    bufferWriter.WriteByte((byte) HexToChar(val));
+                }
+            }
         }
 
         [BurstDiscard]
         public override string ToString()
         {
-            return _utf8String.ToString();
+            var sb = new StringBuilder();
+            
+            for(var i = 0; i < 32; i++)
+            {
+                if (i < 16)
+                {
+                    var shift = i * 4;
+                    var val = (byte) ((_a >> shift) & 0xF);
+                    sb.Append(HexToChar(val));
+                }
+                else
+                {
+                    var shift = (i - 16) * 4;
+                    var val = (byte) ((_b >> shift) & 0xF);
+                    sb.Append(HexToChar(val));
+                }
+            }
+            
+            return sb.ToString();
         }
 
         public bool Equals(Guid other)
@@ -68,6 +99,26 @@ namespace PLUME.Core
         public override int GetHashCode()
         {
             return HashCode.Combine(_a, _b);
+        }
+        
+        private static char HexToChar(byte b)
+        {
+            if (b < 10)
+            {
+                return (char)(b + '0');
+            }
+
+            return (char)(b - 10 + 'A');
+        }
+
+        private static byte CharToHex(char c)
+        {
+            return c switch
+            {
+                >= 'A' and <= 'F' => (byte)(c - 'A' + 10),
+                >= 'a' and <= 'f' => (byte)(c - 'a' + 10),
+                _ => (byte)(c - '0')
+            };
         }
     }
 }

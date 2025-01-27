a1d97ca333371d3f9a64b5774b104877c338543f
Javerliat Charles <charles.javerliat@gmail.com>
2025-01-27 16:32:04 +0100

    Add TODO for RenderSettings hooks

---
+++ Runtime/Base/Hooks/RenderSettingsHooks.cs
@@ -0,0 +1,14 @@
+﻿using PLUME.Core.Hooks;
+using UnityEngine.Scripting;
+
+namespace PLUME.Base.Hooks
+{
+    [Preserve]
+    public class RenderSettingsHooks : IRegisterHooksCallback
+    {
+        public void RegisterHooks(HooksRegistry hooksRegistry)
+        {
+            // TODO
+        }
+    }
+}
